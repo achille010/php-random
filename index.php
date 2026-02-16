3313edb2975f4766cdd3a3a91de44dde9f3972c5
Achille <ishyarugemachille4@gmail.com>
2026-02-16 17:07:30 +0200

    Restructured codebase for better readability and maintainabilitye

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include 'arrays.php';
+    include 'countDown.php';
+    include 'counter.php';
+    include 'drStrange.php';
+    include 'ex.php';
+    include 'factorial.php';
+    include 'fileHandlre.php';
+    include 'formatEmail.php';
+    include 'handle.php';
+    include 'handler.php';
+    include 'sample.php';
+    include 'static.php';
+?>
